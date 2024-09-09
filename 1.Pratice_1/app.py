e76f20e7e079117ec9a90afc0b6fc36fde67baa9
Mohan <siddulamohan1321@gmail.com>
2024-09-09 23:03:52 +0530

    Added the Files

--- 1.Pratice_1/app.py
+++ 1.Pratice_1/app.py
@@ -1,25 +1,25 @@
-###S1- Import Flask
+###----- Import Flask
 
 from flask import Flask
 
 
 
 
-##S2 -  init a Flask object with __name_ Parameter
+##----  init a Flask object with __name_ Parameter
 app = Flask(__name__)
 
 
 
-##S3 - Create an endpoint /route and
+##--- Create an endpoint /route and
 #Bind each route some functionality
 @app.route('/')
 def home():
-    return "Welcome to My Billion Dollor Application $$$$"
+    return "Welcome to My First Flask APP $"
 
 
 
 
-#S4 - Run the app
+#---- Run the app
 
 if __name__ == '__main__':
     app.run(debug = True)
